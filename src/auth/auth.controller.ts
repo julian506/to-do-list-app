8ef85b7baa4c6ad83302887467c70fb23a9e9ca7
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-29 21:34:14 -0500

    Created endpoint for signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Tokens } from './types/tokens.types';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() authDto: AuthDto): Promise<Tokens> {
+    return this.authService.signUp(authDto);
+  }
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,80 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { Tokens } from './types/tokens.types';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async hashData(data: string) {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(data, salt);
+  }
+
+  async updateRefreshToken(userId: number, refresh_token: string) {
+    const hashedRefreshToken = await this.hashData(refresh_token);
+
+    return await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRefreshToken,
+      },
+    });
+  }
+
+  async getTokens(userId: number, email: string) {
+    const access_token = await this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      {
+        secret: this.config.get<string>('ACCESS_TOKEN_SECRET'),
+        expiresIn: '15m',
+      },
+    );
+    console.log(access_token);
+
+    const refresh_token = await this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      {
+        secret: this.config.get<string>('REFRESH_TOKEN_SECRET'),
+        expiresIn: '7d',
+      },
+    );
+
+    return {
+      access_token,
+      refresh_token,
+    };
+  }
+
+  async signUp(authDto: AuthDto): Promise<Tokens> {
+    const hashedPassword = await this.hashData(authDto.password);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: authDto.email,
+        password: hashedPassword,
+      },
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRefreshToken(newUser.id, tokens.refresh_token);
+    return tokens;
+  }
+
+}

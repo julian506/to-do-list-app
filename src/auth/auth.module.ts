1655485a0a9fb4413c4833477b7cb85dd3db2047
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-30 12:21:45 -0500

    Imported jwt strategies as providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -11,6 +13,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
       global: true,
     }),
   ],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, JwtStrategy, JwtRefreshStrategy],
 })
 export class AuthModule {}

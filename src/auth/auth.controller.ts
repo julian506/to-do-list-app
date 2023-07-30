ee12090efab5c9e34b1d1e582c0e6919a1197df2
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-30 11:56:40 -0500

    Made public the existing endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,17 +9,20 @@ import {
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { Tokens } from './types/tokens.types';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('/signup')
   @HttpCode(HttpStatus.CREATED)
   signUp(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signUp(authDto);
   }
 
+  @Public()
   @Post('/signin')
   @HttpCode(HttpStatus.OK)
   signIn(@Body() authDto: AuthDto): Promise<Tokens> {

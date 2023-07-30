51cbdd8f3f5d633f83e788bb6187ce281a6b8d8f
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-30 11:31:39 -0500

    Added authDto for several endpoints

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from '@nestjs/class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
